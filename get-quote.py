50c28933adf0a881aff315eda7155050509f4a38
oceanaut <mavieksen@gmail.com>
2021-08-16 11:28:18 +0300

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,5 +1,9 @@
 def main():
   print("Keep it logically awesome.")
+  
+  secret = (48 6f 77 20 64 69 64 20 79 6f 75 20 64 65 76 65 6c 6f 70 20 74 68 65 20 61 64 76 65 72 73 61 72 69 61 6c 20 6d 69 6e 64 73 65 74 3f 20)
+  secret_as_bytes= b"".join(secret.split())
+  print(secret_as_bytes)
 
   #f = open("quotes.txt")
   #quotes = f.readlines()
